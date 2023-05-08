cb4ce64f65b8d27cb1419045444ff2b6ff1f7436
LelouchCC <tadabanriekoko@gmail.com>
2023-05-08 11:54:23 -0300

    correcao de bug no acessibidade
    bug no js acessibilidade de voz e arrumando css

--- js/acessibilidade_voz.js
+++ js/acessibilidade_voz.js
@@ -5,19 +5,24 @@ let displayImage = document.getElementById('imagem_micro')
 var isOn = true;
 
 function microfone_Imagem(){
-  let displayImage = document.getElementById('imagem_micro')
-  if(displayImage.src.match('./img/acessibilidade/microfone_som.png')){
-    // Defina o texto a ser lido como o conteúdo do elemento abaixo do áudio
-    utterance.text = document.getElementById('texto').textContent;
-    //Inicie a leitura em voz alta
-    window.speechSynthesis.speak(utterance);
-    displayImage.src = './img/acessibilidade/microfone-mudo.png'
-  }
-  else{
-    window.speechSynthesis.cancel();
-    displayImage.src = './img/acessibilidade/microfone_som.png'
-  }
+  if(isOn){
+    //Ao ser clicado a fala ira inicar
+    let displayImage = document.getElementById('imagem_micro')
+    if(displayImage.src.match('./img/acessibilidade/microfone_som.png')){
+      // Defina o texto a ser lido como o conteúdo do elemento abaixo do áudio
+      utterance.text = document.getElementById('texto').textContent;
+      //Inicie a leitura em voz alta
+      window.speechSynthesis.speak(utterance);
+      displayImage.src = './img/acessibilidade/microfone-mudo.png'
+    }
+    else{
+      //Se o microfone for clicado novamente irá parar a fala
+      window.speechSynthesis.cancel();
+      displayImage.src = './img/acessibilidade/microfone_som.png'
+    }
+  }  
   isOn = !isOn;
+  //fala para ao acabar textos
   utterance.addEventListener('end', function() {
     displayImage.src = './img/acessibilidade/microfone_som.png'
 
