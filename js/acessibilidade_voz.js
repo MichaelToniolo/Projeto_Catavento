9f8b22bac1d11d141953ce3ff8a87f0d6934940e
LelouchCC <tadabanriekoko@gmail.com>
2023-05-08 08:29:45 -0300

    update acessibilidade de voz
    correção de bugs e css

--- js/acessibilidade_voz.js
+++ js/acessibilidade_voz.js
@@ -1,32 +1,30 @@
-var utterance = new SpeechSynthesisUtterance();
+const utterance = new SpeechSynthesisUtterance();
+const audioElement = document.querySelector('audio');
+let displayImage = document.getElementById('imagem_micro')
 
-var audioElement = document.querySelector('audio');
-
-var btnLeitura = document.getElementById("toggle-button");
 var isOn = true;
 
-btnLeitura.addEventListener("click", function() {
-  if (isOn) {
-      btnLeitura.innerHTML = "OFF";
-      btnLeitura.classList.add("off");
-      // Defina o texto a ser lido como o conteúdo do elemento abaixo do áudio
-      utterance.text = document.getElementById('texto').textContent;
-
-      // Inicie a leitura em voz alta
-      window.speechSynthesis.speak(utterance);
-  } else {
-      btnLeitura.innerHTML = "ON";
-      btnLeitura.classList.remove("off");
-      
-      window.speechSynthesis.cancel();
-      
+function microfone_Imagem(){
+  let displayImage = document.getElementById('imagem_micro')
+  if(displayImage.src.match('./img/acessibilidade/microfone_som.png')){
+    // Defina o texto a ser lido como o conteúdo do elemento abaixo do áudio
+    utterance.text = document.getElementById('texto').textContent;
+    //Inicie a leitura em voz alta
+    window.speechSynthesis.speak(utterance);
+    displayImage.src = './img/acessibilidade/microfone-mudo.png'
+  }
+  else{
+    window.speechSynthesis.cancel();
+    displayImage.src = './img/acessibilidade/microfone_som.png'
   }
   isOn = !isOn;
   utterance.addEventListener('end', function() {
-  audioElement.pause();
+    displayImage.src = './img/acessibilidade/microfone_som.png'
+
+    audioElement.pause();
   });
-});
-// Adicionar ouvinte de eventos ao objeto window para cancelar a leitura em voz alta e interromper a reprodução de áudio quando a página é descarregada
+}
+// Adicionar ouvinte de eventos ao objeto window para cancelar a leitura em voz alta e interromper a reprodução de áudio quando a página é resetada(F5)
 window.addEventListener('beforeunload', function() {
   window.speechSynthesis.cancel();
   audioElement.pause();
