806facea4a0f334ef75c7eadd4ea69fe5fb1e415
SanduicheMofado <thiagocanabarro10@gmail.com>
2023-05-08 11:24:23 -0300

    Update ChatBot
    Agora com uma "ChatBox"

--- js/rodarodaJequiti.js
+++ js/rodarodaJequiti.js
@@ -1,3 +1,5 @@
+    /* RodaRodaJequiti */
+    
     var x,y,n=0,ny=0,rotINT,rotYINT
     function rotateDIV()
     {
@@ -23,3 +25,48 @@
     x=document.getElementById("rotate2D")
     clearInterval(rotINT)
     }
+
+        /* CHATBOT */
+        
+    var textarray = [
+
+        "BIIRRRRLLLL",
+        
+        "Olá",
+        
+        "Precisa de ajuda?",
+        
+        ];
+        
+        //"Aleatoriza" o texto
+
+        function Texto() {
+        
+        var numr= Math.floor(Math.random()*textarray.length);
+        
+        document.getElementById('ChatBox').innerHTML=textarray[numr];
+        
+        }
+        
+        //Intervalo entre mensagens \\ 1000-1seg
+
+        var inter = setInterval(function() { Texto(); }, 2000);
+
+        var inter = setInterval(function() { Magica(); }, 2000);
+
+        //Carrega função ao carregar a página
+
+        onload = function() { Texto(); }
+
+        onload = function() {Magica(); }
+
+        //Mostra/Oculta mensagem//
+
+        function Magica() {
+            var x = document.getElementById("ChatBox");
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          }
